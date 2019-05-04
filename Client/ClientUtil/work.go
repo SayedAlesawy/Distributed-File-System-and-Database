d9bfa7e0bc0e0279a525a82ffd9f8ffa6878aeac
SayedAlesawy <sayedalesawy14@gmail.com>
2019-05-04 19:41:07 +0200

    Cleaned up client's main

---
+++ Client/ClientUtil/work.go
@@ -0,0 +1,123 @@
+package client
+
+import (
+	fileutils "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/File"
+	logger "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/Log"
+	request "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/Request"
+	"strconv"
+	"strings"
+
+	"fmt"
+)
+
+// UploadHandler A function to handle a upload request
+func (clientObj *client) UploadHandler(req request.UploadRequest) {
+	exists := fileutils.IsThere(req.FileName)
+	if exists == false {
+		logger.LogMsg(LogSign, clientObj.id, fmt.Sprintf("%s :no such file or directory", req.FileName))
+		return
+	}
+
+	serializeRequest := request.SerializeUpload(req)
+
+	sendRequestStatus := clientObj.SendRequest(serializeRequest)
+	logger.LogSuccess(sendRequestStatus, LogSign, clientObj.id, "Successfully sent request to tracker")
+	if sendRequestStatus == false {
+		return
+	}
+
+	response, receiveResponseStatus := clientObj.ReceiveResponse()
+	logger.LogSuccess(receiveResponseStatus, LogSign, clientObj.id, fmt.Sprintf("Tracker response: %s", response))
+	if receiveResponseStatus == false {
+		return
+	}
+
+	if response == "All data nodes are offline" {
+		return
+	}
+
+	arr := strings.Fields(response)
+
+	rsendRequestStatus := clientObj.RSendRequestToDN(arr[0], arr[1], serializeRequest)
+	logger.LogSuccess(rsendRequestStatus, LogSign, clientObj.id, "Successfully sent request to DataNode")
+	if rsendRequestStatus == false {
+		return
+	}
+
+	sendDataStatus := clientObj.SendData(req, arr[0], arr[1])
+	logger.LogSuccess(sendDataStatus, LogSign, clientObj.id, "Successfully sent file to DataNode")
+	if sendDataStatus == false {
+		return
+	}
+
+	notification, notificationStatus := clientObj.ReceiveNotification()
+	logger.LogSuccess(notificationStatus, LogSign, clientObj.id, fmt.Sprintf("Tracker confirmation: %s", notification))
+	if notificationStatus == false {
+		return
+	}
+}
+
+// DownloadHandler A function to handle a download request
+func (clientObj *client) DownloadHandler(reqObj request.UploadRequest) {
+	serializeRequest := request.SerializeUpload(reqObj)
+
+	sendRequestStatus := clientObj.SendRequest(serializeRequest)
+	logger.LogSuccess(sendRequestStatus, LogSign, clientObj.id, "Successfully sent request to tracker")
+	if sendRequestStatus == false {
+		return
+	}
+
+	response, receiveResponseStatus := clientObj.ReceiveResponse()
+	logger.LogSuccess(receiveResponseStatus, LogSign, clientObj.id, fmt.Sprintf("Tracker response: %s", response))
+	if receiveResponseStatus == false {
+		return
+	}
+
+	if response == "404: File not found" {
+		return
+	}
+
+	if response == "All source datanodes are offline, try again later" {
+		return
+	}
+
+	arr := strings.Fields(response)
+	chunkCount, _ := strconv.Atoi(arr[0])
+	dataNodeCount := (len(arr) - 1) / 2
+	chunkEach := chunkCount / dataNodeCount
+	start := 0
+	blockID := 1
+	currPort := 1
+
+	done := make(chan bool)
+
+	for i := 1; i < len(arr)-1; i += 2 {
+		if i == len(arr)-2 {
+			chunkEach += (chunkCount % dataNodeCount)
+		}
+
+		reqObj.ClientPort = reqObj.ClientPort[:3] + strconv.Itoa(i)
+		serializeRequest := request.SerializeUpload(reqObj)
+
+		req := serializeRequest + " " + strconv.Itoa(start) + " " + strconv.Itoa(chunkEach)
+
+		start += chunkEach
+
+		rsendRequestStatus := clientObj.RSendRequestToDN(arr[i], arr[i+1]+"1", req)
+		logger.LogSuccess(rsendRequestStatus, LogSign, clientObj.id, "Successfully sent request to DataNode")
+		if rsendRequestStatus == false {
+			return
+		}
+
+		go clientObj.RecvPieces(reqObj, strconv.Itoa(blockID), chunkEach, done)
+
+		blockID++
+		currPort++
+	}
+
+	for i := 0; i < dataNodeCount; i++ {
+		logger.LogMsg(LogSign, clientObj.id, fmt.Sprintf("Thread #%d finished %t", i+1, <-done))
+	}
+
+	fileutils.AssembleFile(reqObj.FileName, reqObj.FileName[:len(reqObj.FileName)-4], reqObj.FileName[len(reqObj.FileName)-4:], dataNodeCount)
+}
--- Client/Launcher/main.go
+++ Client/Launcher/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	client "Distributed-Video-Processing-Cluster/Client/ClientUtil"
 	constants "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/Constants"
-	fileutils "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/File"
 	logger "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/Log"
 	request "Distributed-Video-Processing-Cluster/Distributed-File-System/Utils/Request"
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -49,12 +47,6 @@ func main() {
 		fmt.Scanf("%s", &fileName)
 
 		if requestType == "up" {
-			exists := fileutils.IsThere(fileName)
-			if exists == false {
-				logger.LogMsg(client.LogSign, clientID, fmt.Sprintf("%s :no such file or directory", fileName))
-				continue
-			}
-
 			requestObj := request.UploadRequest{
 				ID:         requestID,
 				Type:       request.Upload,
@@ -64,43 +56,7 @@ func main() {
 				FileName:   fileName,
 			}
 
-			serializeRequest := request.SerializeUpload(requestObj)
-
-			sendRequestStatus := clientObj.SendRequest(serializeRequest)
-			logger.LogSuccess(sendRequestStatus, client.LogSign, clientID, "Successfully sent request to tracker")
-			if sendRequestStatus == false {
-				continue
-			}
-
-			response, receiveResponseStatus := clientObj.ReceiveResponse()
-			logger.LogSuccess(receiveResponseStatus, client.LogSign, clientID, fmt.Sprintf("Tracker response: %s", response))
-			if receiveResponseStatus == false {
-				continue
-			}
-
-			if response == "All data nodes are offline" {
-				continue
-			}
-
-			arr := strings.Fields(response)
-
-			rsendRequestStatus := clientObj.RSendRequestToDN(arr[0], arr[1], serializeRequest)
-			logger.LogSuccess(rsendRequestStatus, client.LogSign, clientID, "Successfully sent request to DataNode")
-			if rsendRequestStatus == false {
-				continue
-			}
-
-			sendDataStatus := clientObj.SendData(requestObj, arr[0], arr[1])
-			logger.LogSuccess(sendDataStatus, client.LogSign, clientID, "Successfully sent file to DataNode")
-			if sendDataStatus == false {
-				continue
-			}
-
-			notification, notificationStatus := clientObj.ReceiveNotification()
-			logger.LogSuccess(notificationStatus, client.LogSign, clientID, fmt.Sprintf("Tracker confirmation: %s", notification))
-			if notificationStatus == false {
-				continue
-			}
+			clientObj.UploadHandler(requestObj)
 
 		} else if requestType == "dwn" {
 			requestObj := request.UploadRequest{
@@ -112,67 +68,7 @@ func main() {
 				FileName:   fileName,
 			}
 
-			serializeRequest := request.SerializeUpload(requestObj)
-
-			sendRequestStatus := clientObj.SendRequest(serializeRequest)
-			logger.LogSuccess(sendRequestStatus, client.LogSign, clientID, "Successfully sent request to tracker")
-			if sendRequestStatus == false {
-				continue
-			}
-
-			response, receiveResponseStatus := clientObj.ReceiveResponse()
-			logger.LogSuccess(receiveResponseStatus, client.LogSign, clientID, fmt.Sprintf("Tracker response: %s", response))
-			if receiveResponseStatus == false {
-				continue
-			}
-
-			if response == "404: File not found" {
-				continue
-			}
-
-			if response == "All source datanodes are offline, try again later" {
-				continue
-			}
-
-			arr := strings.Fields(response)
-			chunkCount, _ := strconv.Atoi(arr[0])
-			dataNodeCount := (len(arr) - 1) / 2
-			chunkEach := chunkCount / dataNodeCount
-			start := 0
-			blockID := 1
-			currPort := 1
-
-			done := make(chan bool)
-
-			for i := 1; i < len(arr)-1; i += 2 {
-				if i == len(arr)-2 {
-					chunkEach += (chunkCount % dataNodeCount)
-				}
-
-				requestObj.ClientPort = requestObj.ClientPort[:3] + strconv.Itoa(i)
-				serializeRequest := request.SerializeUpload(requestObj)
-
-				req := serializeRequest + " " + strconv.Itoa(start) + " " + strconv.Itoa(chunkEach)
-
-				start += chunkEach
-
-				rsendRequestStatus := clientObj.RSendRequestToDN(arr[i], arr[i+1]+"1", req)
-				logger.LogSuccess(rsendRequestStatus, client.LogSign, clientID, "Successfully sent request to DataNode")
-				if rsendRequestStatus == false {
-					continue
-				}
-
-				go clientObj.RecvPieces(requestObj, strconv.Itoa(blockID), chunkEach, done)
-
-				blockID++
-				currPort++
-			}
-
-			for i := 0; i < dataNodeCount; i++ {
-				logger.LogMsg(client.LogSign, clientID, fmt.Sprintf("Thread #%d finished %t", i+1, <-done))
-			}
-
-			fileutils.AssembleFile(requestObj.FileName, requestObj.FileName[:len(requestObj.FileName)-4], requestObj.FileName[len(requestObj.FileName)-4:], dataNodeCount)
+			clientObj.DownloadHandler(requestObj)
 		}
 
 		requestID++
